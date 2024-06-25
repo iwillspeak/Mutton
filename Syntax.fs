b15491a8360f31ecce2151de35a999a702c404c5
Will Speak <lithiumflame@gmail.com>
2024-06-25 08:15:38 +0100

    feat: Initial Syntax Tree Nodes

--- Syntax.fs
+++ Syntax.fs
@@ -1,5 +1,7 @@
 namespace Mutton.Syntax
 
+open Firethorn.Red
+
 type public SyntaxKind =
     | ERROR = -1
 
@@ -24,3 +26,21 @@ module SyntaxKinds =
     let greenToAst =
         function
         | Firethorn.SyntaxKind n -> enum<SyntaxKind> n
+
+
+[<AbstractClass>]
+type public Expression internal () =
+
+    static member FromRaw(node: SyntaxNode) =
+        match node.Kind |> SyntaxKinds.greenToAst with
+        | SyntaxKind.FORM -> Some(new Form(node) :> Expression)
+        | SyntaxKind.ATOM -> Some(new Atom(node))
+        | _ -> None
+
+and Form internal (red: SyntaxNode) =
+    inherit Expression()
+
+    member _.Body = red.Children() |> Seq.choose (Expression.FromRaw)
+
+and Atom internal (red: SyntaxNode) =
+    inherit Expression()
